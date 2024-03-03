c27f85a065d1ea9e8bea76cf6c035ecb1d75566e
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-03-03 23:37:32 +0400

    added new file

---
+++ index.js
@@ -0,0 +1,10 @@
+ var name=prompt('Adiniz:')
+ var age=prompt('Yasiniz:')
+ var AgeInt=parseInt(age)
+
+ if(AgeInt>=18){
+    alert('You can drive a car!')
+ }
+ else alert("You are left with"+ " " +(18-age)+ "years to drive")
+    
+ 
